7f0e3cf380cec31709b5dee9ac46c25bbd8afd6a
RIfatSuleymanov <fc.chelsea11@yandex.ru>
2024-10-02 14:21:38 +0300

    Test

---
+++ src/main/java/frc/robot/Test.java
@@ -0,0 +1,11 @@
+/*------------------------------*/
+/* Author: Rifat Suleymanov     */
+/* Date: 2024-10-02 14:19       */
+/*------------------------------*/
+package frc.robot;
+
+public class Test {
+    public void Exit(){
+        System.out.println("Exit");
+    }
+}
